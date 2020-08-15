334960e392f089e77988425e88fb663793d78cb4
Achsuthan <achsuthan@icloud.com>
2020-08-15 12:08:56 +0530

    1. Logic work added

--- Views/Home/Home.js
+++ Views/Home/Home.js
@@ -1,50 +1,136 @@
-import React from 'react';
-import {StyleSheet,Text, View} from 'react-native'
-import Card from '../../Components/Card/Card'
+import React, { useEffect, useState } from 'react';
+import SingleRow from '../../Components/Card/SingleRow'
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export default function Home(props){
+export default function Home(props) {
+    const [CARD_PARIS_VALUE, setCardParisValue] = useState([])
+    const [count, setCount] = useState(0)
+
+    const ROW_ARRY = [1, 2, 3, 4]
+    const NO_OF_COLUMN = 3
+
+    useEffect(() => {
+        initiate()
+    }, []);
+
+    useEffect(() => {
+
+    }, [])
+
+    const initiate = () => {
+        var arr = [];
+        while (arr.length < 6) {
+            var r = Math.floor(Math.random() * 100) + 1;
+            if (arr.indexOf(r) === -1) arr.push(r);
+        }
+        arr = [...arr, ...arr]
+        arr = shuffle(arr)
+
+        arr = arr.map((val, index) => {
+            return {
+                number: val,
+                isShow: false,
+                isDone: false
+            }
+        })
+        setCount(0)
+        setCardParisValue(arr)
+    }
+
+
+    const shuffle = (a) => {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    const handleSingleCard = (index) => {
+        let countTMp = count
+        countTMp += 1
+        setCount(countTMp)
+        console.log(index)
+        let tmp = CARD_PARIS_VALUE
+        tmp[index].isShow = !tmp[index].isShow
+        // setCardParisValue([...tmp])
+
+
+        let tmp_2 = tmp.filter((val, mapIndex) => {
+            if (val.isShow && !val.isDone) {
+                let tTmp = val
+                tTmp["index"] = mapIndex
+                return tTmp
+            }
+        })
+        console.log("test2", tmp_2)
+        if (tmp_2.length >= 2) {
+            if (tmp_2[0].number != tmp_2[1].number) {
+                tmp[tmp_2[0].index].isShow = false
+                tmp[tmp_2[1].index].isShow = false
+            }
+            else {
+                console.log(tmp_2)
+                tmp[tmp_2[0].index].isDone = true
+                tmp[tmp_2[1].index].isDone = true
+            }
+        }
+
+        let valueTmep = tmp.filter((val, index) => {
+            return !val.isDone ? val : ""
+        })
+        console.log("valueTmep", valueTmep)
+        if (valueTmep.length == 0) {
+            console.log("done")
+            alert("you completed")
+        }
+        setTimeout(() => {
+            4
+            console.log("Hey singtel")
+            setCardParisValue([...tmp])
+        }, 3000);
+    }
+
     return (
-        < View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        </View>
-        </ View>
+        <>
+            <View style={styles.topView}>
+                <TouchableOpacity onPress={e => initiate()}>
+                    <View>
+                        <Text style={styles.reSetButton}>Restart</Text>
+                    </View>
+                </TouchableOpacity>
+                <Text style={styles.steps}>STEPS:<Text style={styles.counterText}>{count}</Text></Text>
+            </View>
+            {
+
+                ROW_ARRY.map((val, index) => (
+                    <SingleRow key={index} mainArray={CARD_PARIS_VALUE} lastIndex={val * NO_OF_COLUMN - 1} noOfColum={NO_OF_COLUMN} startIndex={val * NO_OF_COLUMN - 3} handleCardClickRow={e => handleSingleCard(e)}></SingleRow>
+                ))
+            }
+        </>
     )
 }
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      padding: 10
+    topView: {
+        height: 50,
+        flexDirection: "row",
+        justifyContent: "space-between"
     },
-    box: {
-      width: 100,
-      height: 100,
-      backgroundColor: 'green',
+    reSetButton: {
+        color: '#35a7ff',
+        fontSize: 20,
+        paddingLeft: 30
     },
-    wrapper: {
-      marginVertical: 10, alignItems: 'center'
+    counterText: {
+        fontSize: 40,
+        color: '#35a7ff',
+    },
+    steps: {
+        color: '#fff',
+        paddingRight: 20,
+        fontSize: 30
     }
-  });
+})
